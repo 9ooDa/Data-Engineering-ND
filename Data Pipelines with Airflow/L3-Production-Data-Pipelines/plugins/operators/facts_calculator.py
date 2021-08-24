0731e6bc61c7686c7661f63d89cc133e5c3c609e
Dayeon <dayeonkukr@gmail.com>
2021-08-24 20:16:48 +0800

    exercise/project

--- Data Pipelines with Airflow/L3-Production-Data-Pipelines/plugins/operators/facts_calculator.py
+++ Data Pipelines with Airflow/L3-Production-Data-Pipelines/plugins/operators/facts_calculator.py
@@ -31,14 +31,27 @@ class FactsCalculatorOperator(BaseOperator):
         #
         # TODO: Set attributes from __init__ instantiation arguments
         #
+        self.redshift_conn_id = redshift_conn_id
+        self.origin_table = origin_table
+        self.destination_table = destination_table
+        self.fact_column = fact_column
+        self.groupby_column = groupby_column
 
     def execute(self, context):
         #
         # TODO: Fetch the redshift hook
         #
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
         #
         # TODO: Format the `facts_sql_template` and run the query against redshift
         #
+        formatted_sql = FactsCalculatorOperator.facts_sql_template.format(
+            destination_table = self.destination_table,
+            groupby_column = self.groupby_column,
+            fact_column = self.fact_column,
+            origin_table = self.origin_table
+        )
+        redshift_hook.run(formatted_sql)
 
         pass
